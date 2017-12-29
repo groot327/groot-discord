ce0c95b956eb0dca4f11b39c889c9f175d7b9ddb
groot327 <groot327@users.noreply.github.com>
2017-12-29 10:57:05 -0500

    Delete reconnecting.js

--- reconnecting.js
+++
@@ -1,4 +0,0 @@
-exports.run = (bot) => {
-  console.log(`Reconnected at ${new Date()}.\nLogged In.\nReady to Serve:\n User:     ${bot.user.tag}\n ID:       ${bot.user.id}`);
-  console.log(` Channels: ${bot.channels.size}\n Servers:  ${bot.guilds.size}\n Users:    ${bot.users.size}`);
-}
