7c87eb663fbd88c57da2afc77d4becf890e2a633
groot327 <groot327@users.noreply.github.com>
2017-12-29 10:57:12 -0500

    Delete ready.js

--- ready.js
+++
@@ -1,4 +0,0 @@
-exports.run = (bot) => {
-  console.log(`\nConnected at ${new Date()}!\nLogged In.\nReady to Serve:\n User:     ${bot.user.tag}\n ID:       ${bot.user.id}`);
-  console.log(` Channels: ${bot.channels.size}\n Servers:  ${bot.guilds.size}\n Users:    ${bot.users.size}`);
-}
